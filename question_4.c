7d9385a1d775a11703f78d6b1861c7270ef22cb4
josephsaju <56636016+josephsaju@users.noreply.github.com>
2019-10-27 22:51:56 +0530

    Create question_4.c

---
+++ question_4.c
@@ -0,0 +1,17 @@
+we are using two rand() function in this program.Program generates 2number .
+    As the program leads 
+"dice1=1+rand()%6" .
+Here one value is generated and it is similarly for the "dice2".
+    Switch statement we can found sum of dice1 and dice 2 values are passed.
+In the switch statement if the value of sum is 11 then the game satus will be won.
+   If the sum is 12 then the game satus will be also won. If no case is executed default case will executed. Her the game satus will be continue and my point will be sum.
+         From this the whole loop start to execute
+If continue=gamesatus. In here the two loops are generated using rand()function will mentioned in the beginning and it will be stored in the dice 1 and dice 2. After sum of dice1 and dice2 will be calculated. 
+     Then the sum and mypoint will be compared with If statement then If the condition is true
+then the game satus will be won inside the else another if statement will check whether the sum is 7 ,If it true the game satus will be lost. And it will change the value of game satus .
+       Here the condition inside the loop is false 
+and the condition will not run again.
+      In case   if(sum==mypoint) is false then the loop will continue till the condition is true .
+    As the program leads another if statement
+If(won=gamesatus)
+          If this statement is false then the contestent will loose the match and if this condition is satisfied   then he will wins
